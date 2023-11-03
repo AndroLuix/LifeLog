31305031d31e8430f26bce507dbb4f2b19506e1e
luigi iadicola <luigiadicola@gmail.com>
2023-11-03 21:20:24 +0100

    r

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,40 +1,51 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+    public function index(){
+        $title = "Login";
+        return view("welcome",compact("title"));
     }
+
+    public function login(Request $request){
+       $credentials =  $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ] );
+
+        if(Auth::attempt($credentials)){
+            return redirect()->route('homepage');
+        }else{
+            return redirect()->route('login')->with('error', 'Invalid credentials');
+        }
+    }
+
+    /**
+     * function for reset password
+     */
+
+    public function forgotPassword(){
+        $title = 'Reset Password';
+       
+        return view('forgot-password',compact('title'));
+
+    }
+
+    public function resetPassword(Request $request){
+        $email = auth()->user();
+
+        dd($email);
+
+       // auth()->user()->update(['password'=> bcrypt($request->password)]);
+
+       // return redirect('/forgot-password')->with('success', 'You\'ve just changed your password');
+    }
+
+    
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,71 +3,42 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function create()
     {
-        $this->middleware('guest');
+        $title = "Register";
+        return view('Auth.register',compact('title'));
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+    public function store(Request $request)
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        $request->validate([
+            'name' => 'required',
+            'lastName'=>'required',
+            'gender'=>'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8|same:password_confirmation',
         ]);
-    }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+        
+       
+        $user = User::create([
+            'name' => $request->name,
+            'lastName'=> $request->lastName,
+            'gender'=> $request->gender,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
-    }
+
+        Auth::login($user);
+        return redirect(RouteServiceProvider::HOME);
+
+        }
 }
